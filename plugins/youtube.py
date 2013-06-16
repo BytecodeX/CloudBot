8dee99080d4bbf6413b650bb81790ba69bf17335
Giorgio Andronico <techdevelop.x@gmail.com>
2013-06-16 17:34:35 +0300

    Correct URL

--- plugins/youtube.py
+++ plugins/youtube.py
@@ -4,13 +4,13 @@ import time
 from util import hook, http
 
 
-youtube_re = (r'(?:youtube.*?(?:v=|/v/)|youtu\.be/|yooouuutuuube.*?id=)'
+youporn_re = (r'(?:youporn.*?(?:v=|/v/)|youpo\.rn/|yooouuupooorn.*?id=)'
               '([-_a-zA-Z0-9]+)', re.I)
 
-base_url = 'http://gdata.youtube.com/feeds/api/'
+base_url = 'http://gdata.youporn.com/feeds/api/'
 api_url = base_url + 'videos/{}?v=2&alt=jsonc'
 search_api_url = base_url + 'videos?v=2&alt=jsonc&max-results=1'
-video_url = "http://youtu.be/%s"
+video_url = "http://youpo.rn/%s"
 
 
 def get_video_description(video_id):
@@ -61,16 +61,16 @@ def GetInHMS(seconds):
     return "%02d:%02d:%02d" % (hours, minutes, seconds)
 
 
-@hook.regex(*youtube_re)
-def youtube_url(match):
+@hook.regex(*youporn_re)
+def youporn_url(match):
     return get_video_description(match.group(1))
 
 
 @hook.command('yt')
 @hook.command('y')
 @hook.command
-def youtube(inp):
-    "youtube <query> -- Returns the first YouTube search result for <query>."
+def youporn(inp):
+    "youporn <query> -- Returns the first YouPorn search result for <query>."
 
     request = http.get_json(search_api_url, q=inp)
 
